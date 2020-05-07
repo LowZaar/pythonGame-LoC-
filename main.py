ba91587c97809eb3a0c017e391418dcbced52e5e
lowzaar <watchmefail12@gmail.com>
2020-05-07 20:15:00 -0300

    new changes

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import random,time
 
+def splashscreen():
+    print(r"""\
+
+          ____                  __      ___           _       
+         |  _ \                 \ \    / (_)         | |      
+         | |_) | ___ _ __ ___    \ \  / / _ _ __   __| | ___  
+         |  _ < / _ \ '_ ` _ \    \ \/ / | | '_ \ / _` |/ _ \ 
+         | |_) |  __/ | | | | |    \  /  | | | | | (_| | (_) |
+         |____/ \___|_| |_| |_|     \/   |_|_| |_|\__,_|\___/ 
+                                                      
+                                                      
+                    """)
+
+
 def startjogo():
-    print("Bem-Vindo")
-    print("Pressione qualquer tecla para jogar")
-    input()
-    return True
+
+    print("Pressione qualquer tecla para jogar, pressione X para fechar o jogo")
+    escolha = input()
+    if escolha == "x":
+        return False
+    else:
+        return True
 
 def escolhajogo(escolha):
     if escolha == 1:
@@ -16,13 +33,56 @@ def escolhajogo(escolha):
     elif escolha == 4:
         return 4
 
+def ataquePlayer():
+    atkdmg = [0, 35, 70, 100]
+    atk = random.choice(atkdmg)
+    return atk
+
+def ataqueNPC():
+    atkdmg = [0, 15, 20]
+    atk = random.choice(atkdmg)
+    return atk
+
+def batalha(enemyHP, playerHP):
+    turno = 1
+    while enemyHP > 0 and playerHP > 0:
+        if turno == 1:
+            atk = ataquePlayer()
+            enemyHP -= atk
+            if enemyHP < 0:
+                enemyHP = 0
+
+            print("Voce deu {} de dano, o inimigo tem {} de vida".format(atk, enemyHP))
+            turno += 1
+        if turno == 2 and enemyHP > 0:
+            turno -= 1
+            atkNPC = ataqueNPC()
+            playerHP -= atkNPC
+            if playerHP < 0:
+                playerHP = 0
+
+            print("O Inimigo lhe causou {} de dano, voce ainda tem {} de vida".format(atkNPC,
+                                                                                      playerHP))
+    if enemyHP == 0:
+        print("Voce Ganhou a batalha, lhe resta {} de vida".format(playerHP))
+    elif playerHP == 0:
+        print("Você morreu, o inimigo ainda vive com {} de vida".format(enemyHP))
+        time.sleep(3.0)
+        return False
+
+def batalharandom():
+    batalhatrue = random.randint(0, 100)
+    return batalhatrue
 
 
 
 
 
+
+splashscreen()
 start = startjogo()
 clockStart = time.time()
+playerHP = 100
 
 while start != False:
 
@@ -40,7 +100,20 @@ while start != False:
         time.sleep(3.0)
         startjogo()
     elif escolhaResult == 2:
-        print("Voce Derrotou o Inimigo")
+        batalhachance = batalharandom()
+        print(batalhachance)
+        if batalhachance <= 65:
+
+            print("Um inimigo está a sua frente")
+            enemyHP = 100
+
+            turno = 1
+            print("Você o ataca")
+            batalha1 = batalha(enemyHP, playerHP)
+
+            if batalha1 == False:
+                start = False
+
     elif escolhaResult == 3:
         print("O inimigo Escapou")
     elif escolhaResult == 4:
@@ -48,7 +121,10 @@ while start != False:
     else:
         print("Escolha uma opção válida")
 
-
+else:
+    print("")
+    print("")
+    startjogo()
 
 
 
